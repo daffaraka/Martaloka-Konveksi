4e153bb96558b38ffa406e040ff4a782bc382ab1
yanmerta <102340574+yanmerta@users.noreply.github.com>
2024-10-17 20:27:56 +0800

    perbaikian footer

--- resources/views/home/partials/home-footer.blade.php
+++ resources/views/home/partials/home-footer.blade.php
@@ -15,11 +15,11 @@
                             </div>
                             <div class="our-company-info">
                                 <div class="text-box">
-                                    <p class="text-justify">Martaloka Konveksi - Menyediakan solusi 
-                                       konveksi berkualitas tinggi dengan dedikasi 
-                                       terhadap kepuasan pelanggan. Kami memproduksi 
-                                       berbagai macam pakaian dengan standar kualitas 
-                                       terbaik. Hubungi kami untuk kebutuhan konveksi Anda.</p>
+                                    <p class="text-justify">Martaloka Konveksi - Menyediakan solusi
+                                        konveksi berkualitas tinggi dengan dedikasi
+                                        terhadap kepuasan pelanggan. Kami memproduksi
+                                        berbagai macam pakaian dengan standar kualitas
+                                        terbaik. Hubungi kami untuk kebutuhan konveksi Anda.</p>
                                 </div>
                                 <ul>
                                     <li>
@@ -27,7 +27,9 @@
                                             <span class="icon-map"></span>
                                         </div>
                                         <div class="text">
-                                            <a href="#">Peta Lokasi</a>
+                                            <a
+                                                href="https://www.google.com/maps/place/UD.+Martaloka+2/@-8.1988126,114.3560445,10z/data=!4m10!1m2!2m1!1smartaloka!3m6!1s0x2dd1833cf334ad25:0x6163d8b1f98db6a4!8m2!3d-8.1988126!4d114.9657857!15sCgltYXJ0YWxva2GSAQpwcmludF9zaG9w4AEA!16s%2Fg%2F11hbqlrhvb?entry=ttu&g_ep=EgoyMDI0MTAxNC4wIKXMDSoASAFQAw%3D%3D">Peta
+                                                Lokasi</a>
                                         </div>
                                     </li>
                                     <li>
@@ -35,7 +37,7 @@
                                             <span class="icon-chat"></span>
                                         </div>
                                         <div class="text">
-                                            <a href="contact.html">Request Desain</a>
+                                            <a href="{{ route('home.daftarCustom') }}">Request Desain</a>
                                         </div>
                                     </li>
                                 </ul>
@@ -54,19 +56,16 @@
                             <div class="footer-widget-links">
                                 <ul>
                                     <li>
-                                        <a href="#">
+                                        <a
+                                            href="https://www.instagram.com/konveksi_martaloka?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
                                             <span class="icon-right-arrow-2"></span>Instagram
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="#">
+                                        <a href="https://www.facebook.com/sedana.sedana.98">
                                             <span class="icon-right-arrow-2"></span>Facebook
                                         </a>
                                     </li>
-                                    
-                                    
-
-
                                 </ul>
                             </div>
                         </div>
@@ -105,13 +104,13 @@
                     </div>
                     <div class="footer-logo-style1">
                         <img style="height: 70px" src="{{ asset('assets/images/logoo2.png') }}" alt="Aska Logo"
-                        title="">
+                            title="">
                     </div>
                     <div class="footer-menu">
                         <ul>
-                            <li><a href="#">Tentang Kami</a></li>
-                            <li><a href="#">Kategori</a></li>
-                            <li><a href="#">Kontak kami</a></li>
+                            <li><a href="{{ route('home.tentang-kami') }}">Tentang Kami</a></li>
+                            <li><a href="{{ route('beranda') }}">Kategori</a></li>
+                            <li><a href="{{ route('home.kontak') }}">Kontak kami</a></li>
                         </ul>
                     </div>
                 </div>
@@ -142,14 +141,6 @@
                     </fieldset>
                 </div>
             </form>
-            <h3>Recent Search Keywords</h3>
-            <ul class="recent-searches">
-                <li><a href="index.html">Campus</a></li>
-                <li><a href="index.html">Event</a></li>
-                <li><a href="index.html">Research</a></li>
-                <li><a href="index.html">Training</a></li>
-                <li><a href="index.html">Education</a></li>
-            </ul>
         </div>
     </div>
 </div>
